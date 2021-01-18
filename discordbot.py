a8f27dbdd98ae927c0c149400d6022a30dafb43d
dynakjp <77594764+dynakjp@users.noreply.github.com>
2021-01-18 20:42:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -36,7 +36,7 @@ async def on_message(message):
         while copy_message.content != '/fin':
             copy_message=await client.wait_for("message",check=check)
             if copy_message.channel==message.channel:
-                await channel.send(">{0}\n[**{1}**]__***{2}***__\n{3}".format(copy_message.id,copy_message.channel.name,copy_message.author.name,copy_message.content))
+                await channel.send("[**{1}**]> __***{2}***__\n{3}".format(copy_message.channel.name,copy_message.author.name,copy_message.content))
         await message.channel.send('終了しました')
         await channel.send('終了しました')
 
