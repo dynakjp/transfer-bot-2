8722db18595b11319385d5c0141717a903294bd1
dynakjp <77594764+dynakjp@users.noreply.github.com>
2021-01-18 12:05:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,44 @@
-from discord.ext import commands
+# インストールした discord.py を読み込む
+import discord
 import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+CID = 725325208537530370
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    #条件に当てはまるメッセージかチェックし正しい場合は返す
+    def check(msg):
+        return msg.author == message.author
+    
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/here to':
+        await message.channel.send('ここが送信元!送信先を教えてね！')
+        wait_message=await client.wait_for("message",check=check)
+        CID=int(wait_message.content)
+        channel = client.get_channel(CID)
+        await channel.send('送信先として設定')
+        copy_message=wait_message
+        while copy_message.content != '/fin':
+            copy_message=await client.wait_for("message",check=check)
+            if copy_message.channel==message.channel:
+                await channel.send("[{0}]{1}\n{2}".format(copy_message.channel.name,copy_message.author.name,copy_message.content))
+        await message.channel.send('終了しました')
+        await channel.send('終了しました')
 
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
